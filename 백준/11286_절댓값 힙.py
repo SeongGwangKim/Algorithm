4c9f907e0707fba39a64456c8817aa26653a4c67
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2022-01-24 14:03:05 +0900

    백준 알고리즘 문제 heapq를 활용한 풀이 max_heap, min_heap을 활용한 풀이

--- 백준/11286_절댓값 힙.py
+++ 백준/11286_절댓값 힙.py
@@ -12,6 +12,8 @@
 출력
 입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 절댓값이 가장 작은 값을 출력하라고 한 경우에는 0을 출력하면 된다.
 '''
+'''
+# heapq, ()튜플을 활용한 내 풀이
 import heapq as hq
 import sys
 
@@ -29,6 +31,7 @@ for _ in range(N):
 
     # 들어온 값이 0이면 값을 출력한다.
     if temp == 0:
+    # 내가 풀어쓴 것 == print(hq.heappop(pq)[1] if pq else 0)
         if pq:
             # pq에 값이 존재하면 pq의 가장 앞의 값의 튜플 중 2번째 값을 출력한다.
             print(hq.heappop(pq)[1])
@@ -38,5 +41,38 @@ for _ in range(N):
     else:
         # 들어온 값을 튜플 구조로 (절댓값, 원래값)을 리스트로 추가한다.
         hq.heappush(pq, (abs(temp), temp))
+'''
+# max_heap, min_heap 활용한 풀이
+import heapq as hq
+import sys
 
+# 처음에 값을 입력받는다.
+# 빠른 입력을 사용한다.
+N = int(sys.stdin.readline().rstrip())
+min_heap = [] # 양수 받아서 활용
+max_heap = [] # 음수 받아서 활용
 
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x:
+        if x > 0:
+            hq.heappush(min_heap, x)
+        else:
+            hq.heappush(max_heap, -x)
+    else:
+        if min_heap:
+            if max_heap:
+                if min_heap[0] < abs(-max_heap[0]):
+                    print(hq.heappop(min_heap))
+                else:
+                    print(-hq.heappop(max_heap))
+
+            else:
+                print(hq.heappop(min_heap))
+
+        else:
+            if max_heap:
+                print(-hq.heappop(max_heap))
+
+            else:
+                print(0)
